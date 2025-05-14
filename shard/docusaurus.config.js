3a340ad33ebf7aee460fa629d41fe8322aa95a16
newwil <fxfusiongames@gmail.com>
2025-05-13 23:12:12 -0500

    Update docusaurus.config.js

--- shard/docusaurus.config.js
+++ shard/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   favicon: "img/FavIcon.png",
 
   // Set the production url of your site here
-  url: "https://luminalabs-games.github.io/",
+  url: "https://luminalabs-games.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/ShardDocumentation/",
