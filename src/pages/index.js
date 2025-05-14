f0e496811efee71cb587007436f7fbca3426ce42
Gvistic <gvistic@gmail.com>
2025-05-13 21:50:00 -0700

    Meta tag fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Shard is a framework for efficient game development in Roblox.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
