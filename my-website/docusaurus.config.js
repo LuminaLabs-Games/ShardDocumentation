ca511d936f50f8529f632e08d8fd3c99ff14922f
newwil <fxfusiongames@gmail.com>
2025-05-13 20:17:23 -0500

    Version Correction + Config Updates & Some wording fixes

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -17,15 +17,15 @@ const config = {
   favicon: "img/FavIcon.png",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://shard.luminalabs.games",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "LuminaLabs Games", // Usually your GitHub org/user name.
+  projectName: "Shard", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
